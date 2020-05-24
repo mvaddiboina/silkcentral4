c5cefb156177f35de11cc3f610598bc08e072688
Madhusudhan Reddy Vaddiboina <madhureddy97@gmail.com>
2020-05-24 12:59:19 +0530

    114

--- TestProcess/ProcessBat/process.bat
+++ TestProcess/ProcessBat/process.bat
@@ -1,3 +1,1 @@
-echo off
-set qsuite=%1
-"C:\Users\user\AppData\Local\UiPath\app-20.4.1\UiRobot.exe" -file %qsuite%
+"C:\Users\user\AppData\Local\UiPath\app-20.4.1\UiRobot.exe" -file "D:\temp\Main.xaml"
